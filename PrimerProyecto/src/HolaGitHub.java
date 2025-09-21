dbf58d87fac535defc02399c08cfd9ebccaee4a1
Patrix-sys <patricia.rita.perez@gmail.com>
2025-09-21 02:38:52 +0200

    Add files via upload

---
+++ PrimerProyecto/src/HolaGitHub.java
@@ -0,0 +1,11 @@
+
+public class HolaGitHub  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("¡Hola GitHub desde Zaragoza!");
+		
+	}
+
+}
